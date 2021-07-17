f99acbb89820a4a62b84ab9b9ef9620c9d42f114
RedAura <86167689+MoeZilla@users.noreply.github.com>
2021-07-17 22:32:22 +0530

    Update types.go

--- googleTLgo/types.go
+++ googleTLgo/types.go
@@ -1,3 +1,12 @@
 package googleTLgo
 
 import "github.com/MoeZilla/googleTLgo/googleTLgo"
+
+type TranslateHelper struct {
+	Text string
+	MainLang string
+	Lang string
+}
+
+type TLHelper []interface{}
+
